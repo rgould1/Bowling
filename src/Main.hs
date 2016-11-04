48c95fb0cabba024e767dda6d83cf94cf276c198
Richard Gould <richardkgould@gmail.com>
2016-11-05 00:00:25 +1100

    Add a scoring function and some basic tests to Main

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,23 @@ import Control.Monad
 
 main :: IO ()
 main = do
-  let fs = [Regular 1 2, Regular 10 0, Regular 5 5]
-      Right scorecard = foldM score empty fs
+  let makeSC :: [Frame] -> IO (Scorecard)
+      makeSC fs = either error return $ foldM score empty fs
+
+  scorecard <- makeSC [Regular 1 2, Regular 10 0, Regular 5 5]
+  perfect <- makeSC $ replicate 9 (Regular 10 0) ++ [Final 10 10 10]
+  zero <- makeSC $ replicate 10 (Regular 0 0)
+  thirty <- makeSC $ replicate 9 (Regular 0 0) ++ [Final 10 10 10]
+  twenty <- makeSC $ replicate 9 (Regular 0 0) ++ [Final 5 5 10]
 
   print scorecard
+  print perfect
+  print zero
+  print thirty
+  print twenty
+
+  print $ scoreGame scorecard
+  print $ scoreGame perfect
+  print $ scoreGame zero
+  print $ scoreGame thirty
+  print $ scoreGame twenty
--- src/Scorecard.hs
+++ src/Scorecard.hs
@@ -3,6 +3,7 @@ module Scorecard
     Frame(..),
     Scorecard(),  -- don't expose constructors
     empty, frames, score,
+    scoreGame
 )
 where
 
@@ -57,3 +58,25 @@ score (Scorecard fs) f@(Final a b c) = do
     when (a'/=10 && a'+b'/=10) $ throwError "Final frame can only be used if a strike or spare is bowled inthe last frame of the game"
     when (length fs/=9) $ throwError "Final can only be used for last frame of game"
     return $ Scorecard (fs ++ [f])
+
+data Carry = None | Spare | Strike | DoubleStrike
+
+-- Score game if complete
+scoreGame :: Scorecard -> Maybe Integer
+scoreGame (Scorecard fs) | length fs<10 = Nothing
+                         | otherwise = Just $ fst $ foldl scoreFrame (0,None) fs
+    where
+        carry c a b = case c of
+                        None   -> 0
+                        Spare  -> a
+                        Strike -> a+b
+                        DoubleStrike -> 2*a+b
+        newC c a b | a==10     = case c of
+                                    Strike       -> DoubleStrike
+                                    DoubleStrike -> DoubleStrike
+                                    _            -> Strike
+                   | a+b==10   = Spare
+                   | otherwise = None
+        scoreFrame (s, c) (Regular b1 b2) = (s + b1 + b2 + carry c b1 b2, newC c b1 b2)
+        scoreFrame (s, c) (Final b1 b2 b3) = (s + b1 + b2 + b3 + carry c b1 b2, None)
+                                        
