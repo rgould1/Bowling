ba2e319af520160ed4df7ae1fc6165928b2b0605
Richard Gould <richardkgould@gmail.com>
2016-11-05 10:44:04 +1100

    Improved testing

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
-# Bowling
+## Bowling
 
 API for creating and evaluting ten-pin bopwling scorecards (see [Ten-Pin Bowling Scoring](https://en.wikipedia.org/wiki/Ten-pin_bowling)).
 
-Requirements:
+# Running
+Everything is written in Haskell. The API is all contained in `src/Scorecard.hs`. A simple test driver is in `src/Main.hs`. With [`stack`](http://www.haskellstack.org), running should be just be a matter of `stack build && stack exec Bowling` from the project directory.
+
+# Requirements
 
 1. Create an empty score card
 2. Given a score card, score a frame
 3. Determine if a game is complete - if so, provide the final score
 
-Stretch:
-1. Score intermediate frames, not jsut complete game
+# Stretch
+1. Score intermediate frames, not just complete game
 2. Support for fouls and splits
 3. Functions to edit, e.g. replace a frame, remove last frame
+4. Handle special named runs, e.g. turkey (three strikes in a row)
+5. Read handler for parsing games represented as text, e.g. "10|5/|X|--|25"
 
-Considerations:
+# Considerations
 1. Consider error handling (Either String a  - could easily generalise to arbitrary monad)
 2. Avoid publishing constructor for Scorecard data type so that invariant can be maintained via scoring functions, e.g. each ball and frame total >=0 <=10, max ten frames, special treatment of final frame
--- src/Main.hs
+++ src/Main.hs
@@ -2,26 +2,35 @@ module Main where
 
 import Scorecard
 import Control.Monad
+import Control.Monad.Except
 
+makeSC :: [Frame] -> Either String Scorecard
+makeSC fs = foldM score empty fs
+
+test :: String -> Maybe Integer -> [Frame] -> IO ()
+test desc expected game = do
+    either (\err -> putStrLn $ "FAIL: Couldn't construct scorecard: " ++ err)
+           (\s -> let total = scoreGame s in
+                  if total == expected
+                    then putStrLn $ "SUCCESS: " ++ desc ++ " " ++ show s ++ " has a score of " ++ show expected
+                    else putStrLn $ "FAILED: " ++ desc ++ " " ++ show s ++ " has a score of " ++ show total ++ ". Was expecting " ++ show expected) $ makeSC game
+           
 main :: IO ()
 main = do
-  let makeSC :: [Frame] -> IO (Scorecard)
-      makeSC fs = either error return $ foldM score empty fs
+    --  Test scores are as expected
+    putStrLn "Test some valid games"
+    test "Incomplete game" Nothing [Regular 1 2, Regular 10 0, Regular 5 5]
+    test "Perfect game" (Just 300) $ replicate 9 (Regular 10 0) ++ [Final 10 10 10]
+    test "Nil game" (Just 0) $ replicate 10 (Regular 0 0)
+    test "Final all strikes" (Just 30) $ replicate 9 (Regular 0 0) ++ [Final 10 10 10]
+    test "Final spare" (Just 20) $ replicate 9 (Regular 0 0) ++ [Final 5 5 10]
 
-  scorecard <- makeSC [Regular 1 2, Regular 10 0, Regular 5 5]
-  perfect <- makeSC $ replicate 9 (Regular 10 0) ++ [Final 10 10 10]
-  zero <- makeSC $ replicate 10 (Regular 0 0)
-  thirty <- makeSC $ replicate 9 (Regular 0 0) ++ [Final 10 10 10]
-  twenty <- makeSC $ replicate 9 (Regular 0 0) ++ [Final 5 5 10]
-
-  print scorecard
-  print perfect
-  print zero
-  print thirty
-  print twenty
-
-  print $ scoreGame scorecard
-  print $ scoreGame perfect
-  print $ scoreGame zero
-  print $ scoreGame thirty
-  print $ scoreGame twenty
+    -- Test fail cases
+    putStrLn ""
+    putStrLn "Test some invalid games"
+    test "Check validation function fails on unexpected score" (Just 30) $ replicate 9 (Regular 0 0) ++ [Final 5 5 10]
+    test "Check invalid single ball" Nothing [Regular 20 0]
+    test "Check invalid frame" Nothing [Regular 5 6]
+    test "Check negative" Nothing [Regular (-5) 15]
+    test "Check out of place final frame" Nothing [Final 5 5 5]
+    test "Check too many frames" Nothing $ replicate 11 (Regular 5 5)
